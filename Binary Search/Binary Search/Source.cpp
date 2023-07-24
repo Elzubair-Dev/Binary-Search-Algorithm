f894acb00d7a73ac96a07ffbf8188f874c8cb9d1
El zubair <104657152+Elzubair-Dev@users.noreply.github.com>
2023-07-24 05:46:02 +0200

    Project

---
+++ Binary Search/Binary Search/Source.cpp
@@ -0,0 +1,37 @@
+using namespace std;
+#include<iostream>
+int BSearch(int arr[], int l, int h, int num)
+{
+	int m = (l + h) / 2;
+	if (arr[m] == num)
+	{
+		return m;
+	}
+	else if (arr[m] > num)
+	{
+		h = m - 1;
+	}
+	else if (arr[m] < num)
+	{
+		l = m + 1;
+	}
+	if (l > h)return -1;
+	else
+	BSearch(arr, l, h, num);
+}
+int main()
+{
+	int x = 0;
+	int z[] = { 10,20,30,40 };
+	int n = sizeof(z) / sizeof(z[0]);
+	cout << "Original Array is [";
+	for (int i = 0; i < n; i++) {
+		cout << z[i] << " ";
+	}
+	cout << "]\n\n";
+	cout << "Enter the Number\n";
+	cin >> x;
+	int r = BSearch(z, 0, n - 1, x);
+	if (r == -1)cout << "Number is not exist\n";
+	else cout << "Found in index no: " << r + 1 << "\n";
+}
